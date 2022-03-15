80bf5735f8cfa2f8debb91e85b6c9ec3ec980756
Peter McCusker <petermcc2000@yahoo.com>
2022-03-15 17:19:25 -0500

    WOrking on extracting images

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,24 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	PUD reader("data/puds/ICEBRDGE.PUD", 0x0);
-	reader.Load();
+	//PUD reader("data/puds/ICEBRDGE.PUD", 0x0);
+	//reader.Load();
 
+	std::ifstream file { "data/images/14.rgb", std::ios::binary | std::ios::in };
+	size_t fileLen = 0;
+	file.seekg(0, file.end);
+	fileLen = file.tellg();
+
+	uint16_t* buffer = new uint16_t[fileLen / 2];
+	file.read((char*)buffer, fileLen);
+	std::cout << "Len: " << fileLen << '\n';
+
+	for (int i = 0; i < fileLen; i++) {
+		if (i % 24 == 0 && i > 0)
+			std::cout << "|\n";
+		std::string start = (i % 24 == 0) ? "| " : "";
+		std::cout << start
+				  << (uint16_t)buffer[i] << ' ';
+	}
 	return 0;
 }
